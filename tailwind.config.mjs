c0bda15e4abc093b6f4d636f79d1364b57e96203
Abiria <abiria.dev@gmail.com>
2024-02-02 18:24:17 +0900

    feat: add museomoderno font as a `title` variant

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+	],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				title: 'MuseoModerno Variable',
+			},
+		},
 	},
 	plugins: [],
 }
